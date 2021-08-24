331794391c93b32cf56e041cad3440eba9e575c1
sachinnair26 <sachin.nair32@gmail.com>
2021-08-24 22:55:47 +0530

    update

--- src/filterfunctions.js
+++ src/filterfunctions.js
@@ -3,7 +3,9 @@ const filters  = {
     generalFilter:(params,values,single_person)=>{
             return single_person[params] === values
     },
-  
+    name:(name,single_person)=>{
+        return single_person.name.first === name || single_person.name.last === name 
+    },
     friends:(name,single_person)=>{
         return (single_person.friends.filter(single_friend =>single_friend.name === name).length > 0)
     },
@@ -17,10 +19,14 @@ const filters  = {
       let joined = tags.join("|")
       let regex = new RegExp(`(?:${joined})\\b`,"gi").test(single_person.tags.join(" "))
         return regex
+    },
+    geoData:(geoData,single_person)=>{
+        let [lat,long] = geoData.split(',')
+        return single_person.latitude === lat && single_person.longitude === long
     }
     
 }
-const Variable = ['tags','friends','registeredRange']
+const Variable = ['name','tags','friends','registeredRange','geoData']
 
 const filterFunction = (single_person,param) =>{
    let combi =   Object.keys(param).map(single_element =>Variable.includes(single_element) ? filters[single_element](param[single_element], single_person):filters.generalFilter(single_element,param[single_element],single_person))
