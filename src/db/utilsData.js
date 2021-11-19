41068d893917ef2f3d720538929f18a0092f4cd4
Fabian Garcia <gfabian757@gmail.com>
2021-11-19 11:51:08 -0600

    mongo volume was added to persist data, utilsData file was modified to add new funtions that help to keep db updated, getDeletedUsers func was added to verify what users were deleted from external API

--- src/db/utilsData.js
+++ src/db/utilsData.js
@@ -30,13 +30,21 @@ const User = require("./model/user.model");
 module.exports.updateData = async()=> {
     try{
       const users = await usersCollection();
+      const allDBUsers= await User.find();
+      const deletedUsers = getDeletedUsers(users, allDBUsers);
       
+      if(deletedUsers.length >0){
+          for(const deletedUser of deletedUsers){
+             const removeUser = await User.findOneAndDelete({"_id": deletedUser["_id"]});
+             
+          }
+      }
 
       for (const user of users) {
         const updatedUser = await User.findOneAndReplace({"_id":user["_id"]}, user, {new: true});
-        console.log(updatedUser)
+       
         if(!updatedUser){
-            const newUser = await User.insertOne(user);
+            const newUser = await User.create(user); //if some new user has been added to the collection but the datadb we need to added to mongodb
           
         }
       }
@@ -47,4 +55,15 @@ module.exports.updateData = async()=> {
     catch(err){
       console.log(err);
     }
+}
+
+const getDeletedUsers = (usersCollection, dbCollection)=>{ //this function helps to get all the users were deleted fron the collection API in order to be able to deleted in our local db
+      
+  const deletedUsers =  dbCollection.filter(user => {
+      return !usersCollection.find(dbUser => {
+         return dbUser["_id"] === user["_id"];
+      });
+   });
+
+   return deletedUsers;
 }
